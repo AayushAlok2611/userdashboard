007a38b2efc5a4c23d4e8ebb968dcc098a91edfc
AayushAlok <59266619+AayushAlok@users.noreply.github.com>
2021-06-22 19:59:47 +0530

    Changed header content-type

--- src/Components/Axios.js
+++ src/Components/Axios.js
@@ -3,6 +3,6 @@ import axios from 'axios'
 export const Axios = axios.create({
   baseURL: 'https://mysterious-dawn-87140.herokuapp.com',
   headers: {
-    'content-type': 'application/json',
+    'content-type': 'multipart/form-data',
   },
 })
